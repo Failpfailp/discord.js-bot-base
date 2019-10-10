23e073a5fdd281e276cc57301c6d29da8760f8bd
flammableassassin <43076116+flamableassassin@users.noreply.github.com>
2019-10-10 11:42:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const {loaders,watchers,config} = require('./src/index.js');
 const Discord = require('discord.js');
-
+//getting token//
 if (config.dev_mode) var token = config.token.dev;
 else var token = config.token.main;
-
+//runing bot//
 console.log(`Dev Mode: ${config.dev_mode}`);
 const client = new Discord.Client();
 loaders(client,config);
 watchers(client,config);
-
+//if dev mode is disabled//
 if (!config.dev_mode) {
   //chewey stuff//
   var cheweyBotAnalyticsAPI=require("discord-bot-analytics")
